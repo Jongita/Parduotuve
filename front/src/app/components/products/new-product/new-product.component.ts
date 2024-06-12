00504ca3852e151e5441e2478855d2496d77dba7
Jongita <jurgitajonaityte@gmail.com>
2024-06-12 08:09:38 +0300

    front

---
+++ front/src/app/components/products/new-product/new-product.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ProductsService } from '../../../services/products.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new-product',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './new-product.component.html',
+  styleUrl: './new-product.component.css'
+})
+export class NewProductComponent {
+
+  constructor(private productService:ProductsService, private router:Router){
+
+  }
+
+  public productSubmit(form:NgForm){
+    console.log(form.form.value);
+    this.productService.addProduct(form.form.value).subscribe((data)=>{
+      this.router.navigate(['products', 'list']);
+    })
+    
+  }
+}
